cf9446f11bc83e507d46f392a19a3071f09017c9
Alexey <meleshhhkevich@icloud.com>
2019-11-30 23:11:25 +0300

    Init app.

---
+++ MyPlaces/MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Alexey Meleshkevich on 30.11.2019.
+//  Copyright © 2019 Alexey Meleshkevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
